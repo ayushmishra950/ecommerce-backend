d8cd7a819adf06a9edbe5cebdaf20f1a1d804c49
ayushmishra950 <ayush950mishra@gmail.com>
2025-09-28 13:21:08 +0530

    Update Profile.js

--- app/src/Profile/Profile.js
+++ app/src/Profile/Profile.js
@@ -11,7 +11,7 @@ const Profile = ({ isOpen, onClose }) => {
   
        const information = async() => {
    try {
-    const res = await axios.get('http://localhost:5000/user');
+    const res = await axios.get('https://ecommerce-backend-qotf.onrender.com/user');
     setObj(res.data);
   } catch (err) {
     setObj(null); 
