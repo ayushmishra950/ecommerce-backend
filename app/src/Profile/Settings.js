8d7a92f5ab28a5e2739f60f5c08db1f479f177be
ayushmishra950 <ayush950mishra@gmail.com>
2025-09-28 13:22:42 +0530

    Update Settings.js

--- app/src/Profile/Settings.js
+++ app/src/Profile/Settings.js
@@ -17,7 +17,7 @@ const Settings = () => {
 
     const information = async() => {
    try {
-    const res = await axios.get('http://localhost:5000/user');
+    const res = await axios.get('https://ecommerce-backend-qotf.onrender.com/user');
     setObj(res.data);
   } catch (err) {
     setObj(null); 
