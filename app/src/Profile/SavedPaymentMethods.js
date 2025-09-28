810cba13cac3d92f26cf8432dff4df94ac1af56b
ayushmishra950 <ayush950mishra@gmail.com>
2025-09-28 13:22:20 +0530

    Update SavedPaymentMethods.js

--- app/src/Profile/SavedPaymentMethods.js
+++ app/src/Profile/SavedPaymentMethods.js
@@ -16,7 +16,7 @@ const SavedPaymentMethods = () => {
 
   const information = async() => {
    try {
-    const res = await axios.get('http://localhost:5000/user');
+    const res = await axios.get('https://ecommerce-backend-qotf.onrender.com/user');
     setObj(res.data);
   } catch (err) {
     setObj(null); 
